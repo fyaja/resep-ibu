6456160f8d9724429d66a50fe9e778049d0e8839
Ahmad Fay Baihaqi <ahmadfaybaihaqi@gmail.com>
2024-06-20 09:03:25 +0700

    fix header, add homepage and add some img

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav ul');
+const main = document.querySelector('main');
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('show')
+  main.classList.toggle('margin-top')
 });
 
 const navLinks = document.querySelectorAll('nav ul li a');
 navLinks.forEach((navLink) => {
   navLink.addEventListener('click', () => {
     nav.classList.remove('show')
+    main.classList.remove('margin-top')
   })
 })
